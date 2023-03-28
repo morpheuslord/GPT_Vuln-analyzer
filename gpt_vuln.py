0bfdfe46d09ddd622205b9a35a6d09fe1d01dcaf
morpheuslord <70637311+morpheuslord@users.noreply.github.com>
2023-03-28 21:53:48 +0530

    Update gpt_vuln.py

--- gpt_vuln.py
+++ gpt_vuln.py
@@ -2,15 +2,33 @@ import nmap
 import openai
 import argparse
 
-openai.api_key = "__API__KEY"
+openai.api_key = "__API__KEY__"
 model_engine = "text-davinci-003"
 nm = nmap.PortScanner()
 
-parser = argparse.ArgumentParser(description='Python-Nmap and chatGPT intigrated Vulnerability scanner')
-parser.add_argument('target', metavar='target', type=str, help='Target IP or hostname')
+parser = argparse.ArgumentParser(
+    description='Python-Nmap and chatGPT intigrated Vulnerability scanner')
+parser.add_argument('--target', metavar='target', type=str,
+                    help='Target IP or hostname')
+parser.add_argument('--profile', metavar='profile', type=int, default=1,
+                    help='Enter Profile of scan 1-5 (Default: 1)', required=False)
+
 args = parser.parse_args()
 
 target = args.target
+profile = args.profile
+
+
+def banner():
+    print("""
+ _______ _     _ _______ 
+(_______|_)   (_|_______)
+ _   ___ _     _ _______ 
+| | (_  | |   | |  ___  |
+| |___) |\ \ / /| |   | |
+ \_____/  \___/ |_|   |_|
+                                                      
+    """)
 
 
 def p1(ip):
@@ -18,24 +36,27 @@ def p1(ip):
     json_data = nm.analyse_nmap_xml_scan()
     analize = json_data["scan"]
     # Prompt about what the quary is all about
-    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(analize)
+    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(
+        analize)
     # A structure for the request
     completion = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
-        max_tokens=1024,
+        max_tokens=2500,
         n=1,
         stop=None,
     )
     response = completion.choices[0].text
     return response
 
+
 def p2(ip):
     nm.scan('{}'.format(ip), arguments='-Pn -T4 -A -v')
     json_data = nm.analyse_nmap_xml_scan()
     analize = json_data["scan"]
     # Prompt about what the quary is all about
-    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(analize)
+    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(
+        analize)
     # A structure for the request
     completion = openai.Completion.create(
         engine=model_engine,
@@ -53,7 +74,8 @@ def p3(ip):
     json_data = nm.analyse_nmap_xml_scan()
     analize = json_data["scan"]
     # Prompt about what the quary is all about
-    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(analize)
+    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(
+        analize)
     # A structure for the request
     completion = openai.Completion.create(
         engine=model_engine,
@@ -64,13 +86,15 @@ def p3(ip):
     )
     response = completion.choices[0].text
     return response
+
 
 def p4(ip):
     nm.scan('{}'.format(ip), arguments='-Pn -p- -T4 -A -v')
     json_data = nm.analyse_nmap_xml_scan()
     analize = json_data["scan"]
     # Prompt about what the quary is all about
-    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(analize)
+    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(
+        analize)
     # A structure for the request
     completion = openai.Completion.create(
         engine=model_engine,
@@ -82,12 +106,15 @@ def p4(ip):
     response = completion.choices[0].text
     return response
 
+
 def p5(ip):
-    nm.scan('{}'.format(ip), arguments='-Pn -sS -sU -T4 -A -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script=vuln')
+    nm.scan('{}'.format(
+        ip), arguments='-Pn -sS -sU -T4 -A -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script=vuln')
     json_data = nm.analyse_nmap_xml_scan()
     analize = json_data["scan"]
     # Prompt about what the quary is all about
-    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(analize)
+    prompt = "do a vulnerability analysis of {} and return a vulnerabilty report in json".format(
+        analize)
     # A structure for the request
     completion = openai.Completion.create(
         engine=model_engine,
@@ -99,25 +126,25 @@ def p5(ip):
     response = completion.choices[0].text
     return response
 
+
 def main(target):
-    profile = int(input("Enter profile of scan: "))
-    if profile == 1:
-        final = p1(target)
-        print(final)
-    elif profile == 2:
-        final = p2(target)
-        print(final)
-    elif profile == 3:
-        final = p3(target)
-        print(final)
-    elif profile == 4:
-        final = p4(target)
-        print(final)
-    elif profile == 5:
-        final = p5(target)
-        print(final)
-    else:
-        print('Error has in profile input or target input please check......')
+    banner()
+    match profile:
+        case 1:
+            final = p1(target)
+            print(final)
+        case 2:
+            final = p2(target)
+            print(final)
+        case 3:
+            final = p3(target)
+            print(final)
+        case 4:
+            final = p4(target)
+            print(final)
+        case 5:
+            final = p5(target)
+            print(final)
 
 
 if __name__ == "__main__":
